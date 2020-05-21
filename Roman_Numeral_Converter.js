a8c35be1a32ca9ad3c86d740b8766f3ba5a968af
Sebastian Muñoz <zebax97@gmail.com>
2020-05-20 22:32:53 -0500

    primer commit

---
+++ Roman_Numeral_Converter.js
@@ -0,0 +1,31 @@
+function convertToRoman(num) {
+  const NumbersRoman = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  let accumulator = "";
+
+  for (const key in NumbersRoman) {
+    const numbervalue = NumbersRoman[key];
+
+    while (numbervalue <= num) {
+      num -= numbervalue;
+      accumulator += key;
+    }
+  }
+  return accumulator;
+}
+
+convertToRoman(36);
